3aafd3f4c4f212474d296c953f543bc55ff97e9b
Gavin Lobo <gavinandre2007@gmail.com>
2025-07-17 00:49:37 +0700

    Update listener for port 8080

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -45,10 +45,31 @@ resource "aws_lb_target_group" "backend" {
   })
 }
 
-# ALB Listener
-resource "aws_lb_listener" "backend" {
+# ALB Listener for port 80 - redirects to port 8080
+resource "aws_lb_listener" "http_redirect" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "8080"
+      protocol    = "HTTP"
+      status_code = "HTTP_301"
+    }
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-http-redirect-listener"
+    Type = "lb-listener"
+  })
+}
+
+# ALB Listener for port 8080 - forwards to backend service
+resource "aws_lb_listener" "backend" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "8080"
   protocol          = "HTTP"
 
   default_action {
--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -19,6 +19,14 @@ resource "aws_security_group" "alb" {
   }
 
   ingress {
+    description = "HTTP Alt Port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTPS"
     from_port   = 443
     to_port     = 443
